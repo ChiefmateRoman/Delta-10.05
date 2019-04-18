82e5813f9dc9a1d2b1e388a42ddc990201861fed
Lesnikov Roman <masterlesnikov@gmail.com>
2019-04-18 12:31:40 +0200

    Initial Commit

---
+++ Delta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delta
+//
+//  Created by Roman Lesnikov on 18/04/2019.
+//  Copyright © 2019 Roman Lesnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
